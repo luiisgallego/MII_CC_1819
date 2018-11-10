020ec3e026fd0ca9f105aa9e27900aca6f0e42c6
Luis Gallego Quero <lgaq94@gmail.com>
2018-11-10 17:47:23 +0100

    test completos con 100%. close #12

--- test/app.js
+++ test/app.js
@@ -1,63 +1,243 @@
-// TEST UNITARIO - INTEGRACION
-
 var request = require('supertest'),
 should = require('should'),
 app = require('../index.js');
 
+describe("Comprobamos que no hay Items", function(){
+    it('Debería devolver no hay items', function(done){
+        request(app)
+            .get('/item')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {                    
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    resultado.body.ejemplo.should.have.property('ruta', '/item');
+                    resultado.body.ejemplo.should.have.property('valor', 'No hay Items.');
+                    done(); 
+                } 
+            });
+    });
+});
 
-// COMPROBAR
-    // - TIPO MIME
-    // - ESTADOS
-    // - CONTENIDO
-
-    // HACER UN PUT, Y LUEGO OTRO PUT IGUAL Y VER QUE DEVUELVE
-
-
-
-describe("Añadimos nuevo item", function(){
-
+describe("Añadimos nuevo item (PUT)", function(){
     it('Debería crear el item', function(done){
         request(app)
-            .put('/item/item1/3/100')  
+            .put('/item/prueba1/1/100')  
 	        .expect('Content-Type', /json/)
             .expect(200, done)
     });
-
     it('Debería devolver el ID',function(done){
         request(app)
-            .put('/item/item2/5/300')
+            .put('/item/prueba2/2/200')
             .expect('Content-Type',/json/)
             .expect(200)
             .end(function(error, resultado){
-                if(error) {
-                    return done(error);
-                } else {                    
-                    var res = resultado.body.ejemplo;
-                    var valor = res.valor;
-                    //var valor2 = resultado.body.respuesta.valor
-
-                    resultado.body.should.have.property('status', 'OK'); 
-                    res.should.have.property('ruta', '/item/:nombre/:cantidad/:precio');
-                    valor.should.have.property('ID', 'ID_item2');                   
-                    //console.log("LOG ID:" + valor2.ID);
+                if(error) return done(error);
+                else {                    
+                    //var res = resultado.body.ejemplo;
+                    var valor = resultado.body.ejemplo.valor;
+                    valor.should.have.property('ID', 'ID_prueba2');
                     done();
                 }                
             });
     });
-
-    /*it('Debería devolver el NOMBRE',function(done){
+    it('Debería devolver el item al completo', function(done){
         request(app)
-            .put('/item/item3/6/800')
+            .put('/item/prueba3/3/300')
             .expect('Content-Type',/json/)
             .expect(200)
             .end(function(error, resultado){
-                if(error) {
-                    return done(error);
-                } else {
-                    resultado.body.should.have.property('nombre', 'item3');                    
-                    //console.log("LOG nombre:" + resultado.body.nombre);
-                    done();
+                if(error) return done(error);
+                else {
+                    var valor = resultado.body.ejemplo.valor;
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:nombre/:cantidad/:precio');
+                    // Comprobamos ejemplo -> valor
+                    valor.should.have.property('ID', 'ID_prueba3'); 
+                    valor.should.have.property('nombre', 'prueba3'); 
+                    valor.should.have.property('cantidad', '3'); 
+                    valor.should.have.property('precio', '300');
+                    done(); 
+                }
+            });
+    });   
+    it('Debería devolver item ya existe', function(done){
+        request(app)
+            .put('/item/prueba3/3/300')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {
+                    var valor = resultado.body.ejemplo.valor;
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:nombre/:cantidad/:precio');
+                    resultado.body.ejemplo.should.have.property('valor', 'ITEM ya existe');
+                    done(); 
+                }
+            });
+    }); 
+});
+
+describe("Actualizamos un item (POST)", function(){
+    it('Debería actualizar el item',function(done){
+        request(app)
+            .post('/item/prueba2/21/210')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {                    
+                    var valor = resultado.body.ejemplo.valor;
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:nombre/:cantidad/:precio');
+                    // Comprobamos ejemplo -> valor
+                    valor.should.have.property('ID', 'ID_prueba2'); 
+                    valor.should.have.property('nombre', 'prueba2'); 
+                    valor.should.have.property('cantidad', '21'); 
+                    valor.should.have.property('precio', '210');
+                    done(); 
                 }                
             });
-    });*/
+    });
+    it('Debería devolver item no existe',function(done){
+        request(app)
+            .post('/item/prueba4/21/210')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {                    
+                    var valor = resultado.body.ejemplo.valor;
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:nombre/:cantidad/:precio');
+                    resultado.body.ejemplo.should.have.property('valor', 'ITEM no existe');
+                    done(); 
+                }                
+            });
+    });
+});
+
+describe("Borramos un item (DELETE)", function(){
+    it('Debería borrar el item',function(done){
+        request(app)
+            .delete('/item/ID_prueba2')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {                    
+                    var valor = resultado.body.ejemplo.valor;
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:ID');
+                    done(); 
+                }                
+            });
+    });
+    it('Debería devolver item no existe',function(done){
+        request(app)
+            .delete('/item/ID_prueba2')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {                    
+                    var valor = resultado.body.ejemplo.valor;
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:ID');
+                    resultado.body.ejemplo.should.have.property('valor', 'ITEM no existe');
+                    done(); 
+                }                
+            });
+    });
+});
+
+describe("Realizamos consultas (GET)", function(){
+    it('Debería devolver status OK', function(done){
+        request(app)
+            .get('/')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {                    
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    done(); 
+                } 
+            });
+    });
+    it('Debería devolver los items', function(done){
+        request(app)
+            .get('/item')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {          
+                    var valor = resultado.body.ejemplo.valor;        
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item');
+                    // Comprobamos ejemplo -> valor
+                    valor.ID_prueba1.should.have.property('ID','ID_prueba1');
+                    valor.ID_prueba1.should.have.property('nombre','prueba1');
+                    valor.ID_prueba1.should.have.property('cantidad','1');
+                    valor.ID_prueba1.should.have.property('precio','100');
+                    valor.ID_prueba3.should.have.property('ID', 'ID_prueba3');
+                    done(); 
+                } 
+            });
+    });
+    it('Debería devolver el ID', function(done){
+        request(app)
+            .get('/item/ID_prueba1')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {          
+                    var valor = resultado.body.ejemplo.valor;       
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:ID');
+                    // Comprobamos ejemplo -> valor
+                    valor.should.have.property('ID','ID_prueba1');
+                    done(); 
+                } 
+            });
+    });
+    it('Debería devolver el item no existe', function(done){
+        request(app)
+            .get('/item/ID_prueba2')
+            .expect('Content-Type',/json/)
+            .expect(200)
+            .end(function(error, resultado){
+                if(error) return done(error);
+                else {          
+                    var valor = resultado.body.ejemplo.valor;       
+                    // Comprobamos status
+                    resultado.body.should.have.property('status', 'OK');
+                    // Comprobamos ejemplo -> ruta
+                    resultado.body.ejemplo.should.have.property('ruta', '/item/:ID');
+                    resultado.body.ejemplo.should.have.property('valor', 'ITEM no existe');
+                    done(); 
+                } 
+            });
+    });
 });
--- test/items.js
+++ test/items.js
@@ -1,23 +1,51 @@
-// TEST UNITARIO
-
 var assert = require("assert"),
 items = require(__dirname+"/../items.js");
+//import Items from "/../items.js";
 
 // Variable local
 var item;
 
-describe('Porra', function(){
+describe('Items', function(){
 
+    // Probamos que se haya cargado bien la clase
     describe('Carga', function(){
         it('Deberia estar cargado el  módulo', function(){
             assert(items, "Cargado");
         });
     });
 
-    item = new items.Items("CajaMarron", "6", "500");
+    // Probamos que se cree correctamente cada atributo
+    item = new items("Prueba1", "1", "100");
     describe('ID', function(){
         it('Debería ser correcto el ID', function(){
-            assert.equal(item.ID, "ID_CajaMarron");
+            assert.equal(item.ID, "ID_Prueba1");
+        });    
+    });
+    describe('Nombre', function(){
+        it('Debería ser correcto el Nombre', function(){
+            assert.equal(item.nombre, "Prueba1");
+        });    
+    });
+    describe('Cantidad', function(){
+        it('Debería ser correcta la cantidad', function(){
+            assert.equal(item.cantidad, "1");
+        });    
+    });
+    describe('Precio', function(){
+        it('Debería ser correcto el Precio', function(){
+            assert.equal(item.precio, "100");
+        });    
+    });
+
+    // Probamos las distintas funciones
+    describe('getID', function(){
+        it('Debería ser correcto el ID devuelto', function(){
+            assert.equal(item.getID(), "ID_Prueba1");
+        });    
+    });
+    describe('creaID', function(){
+        it('Debería ser correcto el ID creado', function(){
+            assert.equal(item.creaID("Prueba2"), "ID_Prueba2");
         });    
     });
 });
