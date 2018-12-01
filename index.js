ec7bf40c2e874ae8a27e771cc27f529901042a59
Luis Gallego Quero <lgaq94@gmail.com>
2018-12-01 17:14:33 +0100

    añado conexion BD Mongo. close #28

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 // Creamos las dependencias
 var express = require('express');
-var app = express();
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
 var items = require("./items.js");
+var app = express();
 
 // Variables globales
 var almacenItems = new Object;
 var respuesta = new Object;
+// Var globales BD_Mongo.db
+var ObjectID = mongodb.ObjectID;
+var CONTACTS_COLLECTION = "items";
+var db; // Global para ser utilizada por todas las rutas
 
+// Configuramos puertos y conexiones
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.set('puerto', (process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+// Conectamos BD y lanzamos aplicación
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function(err, client) {
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+
+    // Guardamos el objeto devuelto para utilizarlo en las consultas
+    db = client.db();
+    console.log("Conexión BD establecida.");
+
+    // Lanzamos la aplicacion
+    app.listen(app.get('puerto'), server_ip_address, function() {
+        console.log("Items app corriendo en " + server_ip_address + ":" + app.get('puerto'));
+    });  
+});
+
 
 // Crea un nuevo item
 app.put('/item/:nombre/:cantidad/:precio', function(request, response){
@@ -98,10 +124,6 @@ app.get('/item/:ID', function(request, response){
     // Comprobamos que existe
     if(!almacenItems[identificador]) response.status(404).type('json').send();
     else response.status(200).type('json').send(JSON.stringify(almacenItems[identificador], null, "\t"));   
-});
-
-app.listen(app.get('puerto'), server_ip_address, function() {
-    console.log("Items app corriendo en " + server_ip_address + ":" + app.get('puerto'));
 });
 
 // Exporta la variable para poder hacer tests
