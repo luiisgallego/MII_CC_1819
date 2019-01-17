6d063eee99f9d052cf14e0d6964d9d18163c46a0
Luis Gallego Quero <lgaq94@gmail.com>
2019-01-17 11:02:47 +0100

    intento de conexion. #54

--- index.js
+++ index.js
@@ -24,11 +24,12 @@ app.use(bodyParser.json());
 var uri_mlab = "mongodb://items:items1@ds044587.mlab.com:44587/items";
 var uri_localhost = "mongodb://localhost/itemsTest"
 var uri_new_localhost = "mongodb://localhost:27017/Db";
+var uri_azure = "mongodb://10.0.0.4:27017/Db"
 //var URI_mongo_mlab = uri_mlab || uri_localhost; 
 
-mongoose.connect(uri_new_localhost, { useNewUrlParser: true }, function (err, res) {
-    if(err) logger.info('ERROR conectando a: ' + uri_new_localhost + '. ' + err);
-    else logger.info('BD conectada a: ' + uri_new_localhost);
+mongoose.connect(uri_azure, { useNewUrlParser: true }, function (err, res) {
+    if(err) logger.info('ERROR conectando a: ' + uri_azure + '. ' + err);
+    else logger.info('BD conectada a: ' + uri_azure);
 });
 
 // Creamos y configuramos el sistema de logs
